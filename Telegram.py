c18fbec9598f5cfff5174ddfa0a6f6e08f3f34d0
yingshaoxo <yingshaoxo@gmail.com>
2017-04-12 23:25:41 +0800

    update

--- Telegram.py
+++ Telegram.py
@@ -2,16 +2,23 @@ import sys
 import telebot
 from __Xiaoya__ import xiaoya
 
-x = xiaoya('xiaoya', 17, 'telegram')
+TGx = xiaoya('xiaoya', 17, 'telegram')
+QQx = xiaoya('xiaoya', 17, 'books')
 bot = telebot.TeleBot("121899714:AAF3xShKMc52iV5yN93fiIjOH98ZXP1zcOc")
 
-@bot.message_handler(commands=['start', 'hi'])
+@bot.message_handler(commands=['start'])
 def send_welcome(msg):
     bot.reply_to(msg, "Hi~")
 
+@bot.message_handler(commands=['go'])
+def send_books(msg):
+    result = QQx.reply(msg.text)
+    if result != '':
+        bot.reply_to(msg, result)
+
 @bot.message_handler(content_types=['text'])
 def handle(msg):
-    result = x.reply(msg.text)
+    result = TGx.reply(msg.text)
     if result != '':
         bot.reply_to(msg, result)
 
