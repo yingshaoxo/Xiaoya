6635d0c4fb11d33cf3059f1fddc627f6e3da6080
yingshaoxo <yingshaoxo@gmail.com>
2017-05-20 02:11:52 +0800

    remove class in telegram

--- Telegram.py
+++ Telegram.py
@@ -7,60 +7,58 @@ from __Xiaoya__ import xiaoya
 import multiprocessing
 
 
-class GaoKaoTask():
-    def init(self):
-        user_id = 'telegram'
-        directory = 'GaoKao'
-        TGx = xiaoya('xiaoya', 17, user_id, directory)
+user_id = 'telegram'
+directory = 'GaoKao'
+TGx = xiaoya('xiaoya', 17, user_id, directory)
 
-        GROUP = -1001082405980
-        #GROUP = -178341019
-        TOKEN = '121899714:AAF3xShKMc52iV5yN93fiIjOH98ZXP1zcOc'
-        tb = telebot.AsyncTeleBot(TOKEN)
+GROUP = -1001082405980
+#GROUP = -178341019
+TOKEN = '121899714:AAF3xShKMc52iV5yN93fiIjOH98ZXP1zcOc'
+tb = telebot.AsyncTeleBot(TOKEN)
 
-        start_time = datetime.datetime(2017, 5, 20, 17)
-        work_days = 5 # all the day you got
+start_time = datetime.datetime(2017, 5, 20, 17)
+work_days = 5 # all the day you got
 
-        work_time = datetime.timedelta(hours=work_days*24) 
-        end_time = start_time + work_time #deadline
+work_time = datetime.timedelta(hours=work_days*24) 
+end_time = start_time + work_time #deadline
 
-        all_interval = work_time.total_seconds()
+all_interval = work_time.total_seconds()
 
-        do()         
 
-    def job_interval_time(self):
-        diff = end_time - datetime.datetime.now()
-        now_interval = diff.total_seconds()
 
-        left_num = TGx.left_num()
-        left_day = int(now_interval / (60 * 60 * 24)) - 1
+def job_interval_time():
+    diff = end_time - datetime.datetime.now()
+    now_interval = diff.total_seconds()
+
+    left_num = TGx.left_num()
+    left_day = int(now_interval / (60 * 60 * 24)) - 1
     
-        today_left_seconds = ((end_time - datetime.timedelta(hours=left_day*24)) - datetime.datetime.now()).total_seconds()
+    today_left_seconds = ((end_time - datetime.timedelta(hours=left_day*24)) - datetime.datetime.now()).total_seconds()
 
-        interval_t = ((left_day * (8 * 60 * 60))+today_left_seconds) // left_num
+    interval_t = ((left_day * (8 * 60 * 60))+today_left_seconds) // left_num
     
-        #progress = str((1 - (now_interval/all_interval)) * 100)[:7] + '%'
-        if interval_t < 0:
-            tb.send_message(GROUP, 'Your schedule times up!')
-            exit()
+    #progress = str((1 - (now_interval/all_interval)) * 100)[:7] + '%'
+    if interval_t < 0:
+        tb.send_message(GROUP, 'Your schedule times up!')
+        exit()
         
-        return left_num, interval_t
+    return left_num, interval_t
 
-    def job(self):
-        result = TGx.reply('fuck')
-        if result != '':
-            tb.send_message(GROUP, result)
-        return ''
+def job():
+    result = TGx.reply('fuck')
+    if result != '':
+        tb.send_message(GROUP, result)
+    return ''
 
-    def do(self):
-        while True:
-            left_num, interval_t = job_interval_time()
-            #tb.send_message(131513300, 'You still got {} pieces to read.'.format(left_num))
-            #tb.send_message(131513300, 'Every {} seconds one piece.'.format(str(int(interval_t))))
-            job()
-            time.sleep(interval_t)
+def do():
+    while True:
+        left_num, interval_t = job_interval_time()
+        #tb.send_message(-1001120909649, 'You still got {} pieces to read.'.format(left_num))
+        #tb.send_message(-1001120909649, 'Every {} seconds one piece.'.format(str(int(interval_t))))
+        job()
+        time.sleep(interval_t)
 
-task = multiprocessing.Process(target=GaoKaoTask)
+task = multiprocessing.Process(target=do)
 
 x = xiaoya('xiaoya', 17, 'telegram', '__all__')
 bot = telebot.TeleBot("121899714:AAF3xShKMc52iV5yN93fiIjOH98ZXP1zcOc")
@@ -68,7 +66,7 @@ bot = telebot.TeleBot("121899714:AAF3xShKMc52iV5yN93fiIjOH98ZXP1zcOc")
 @bot.message_handler(commands=['chat_id'])
 def handle(msg):
     if msg.chat.type == 'supergroup':
-        bot.reply_to(msg.chat.id, 'Chat_id of this group:\n\n{}'.format(str(msg.chat.id)))
+        bot.reply_to(msg, 'Chat_id of this group:\n\n{}'.format(str(msg.chat.id)))
 
 @bot.message_handler(commands=['start_task'])
 def handle(msg):
