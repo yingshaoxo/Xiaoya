6665e0136bf10178c88967fef5381f48d819698f
yingshaoxo <yingshaoxo@gmail.com>
2017-05-20 01:50:39 +0800

    multi processing

--- Telegram.py
+++ Telegram.py
@@ -1,33 +1,92 @@
+import datetime
+import time
+
 import telebot
 from __Xiaoya__ import xiaoya
 
-TGx = xiaoya('xiaoya', 17, 'telegram', '__all__')
+import multiprocessing
+
+
+class GaoKaoTask():
+    def init(self):
+        user_id = 'telegram'
+        directory = 'GaoKao'
+        TGx = xiaoya('xiaoya', 17, user_id, directory)
+
+        GROUP = -1001082405980
+        #GROUP = -178341019
+        TOKEN = '121899714:AAF3xShKMc52iV5yN93fiIjOH98ZXP1zcOc'
+        tb = telebot.AsyncTeleBot(TOKEN)
+
+        start_time = datetime.datetime(2017, 5, 20, 17)
+        work_days = 5 # all the day you got
+
+        work_time = datetime.timedelta(hours=work_days*24) 
+        end_time = start_time + work_time #deadline
+
+        all_interval = work_time.total_seconds()
+
+        do()         
+
+    def job_interval_time(self):
+        diff = end_time - datetime.datetime.now()
+        now_interval = diff.total_seconds()
+
+        left_num = TGx.left_num()
+        left_day = int(now_interval / (60 * 60 * 24)) - 1
+    
+        today_left_seconds = ((end_time - datetime.timedelta(hours=left_day*24)) - datetime.datetime.now()).total_seconds()
+
+        interval_t = ((left_day * (8 * 60 * 60))+today_left_seconds) // left_num
+    
+        #progress = str((1 - (now_interval/all_interval)) * 100)[:7] + '%'
+        if interval_t < 0:
+            tb.send_message(GROUP, 'Your schedule times up!')
+            exit()
+        
+        return left_num, interval_t
+
+    def job(self):
+        result = TGx.reply('fuck')
+        if result != '':
+            tb.send_message(GROUP, result)
+        return ''
+
+    def do(self):
+        while True:
+            left_num, interval_t = job_interval_time()
+            #tb.send_message(131513300, 'You still got {} pieces to read.'.format(left_num))
+            #tb.send_message(131513300, 'Every {} seconds one piece.'.format(str(int(interval_t))))
+            job()
+            time.sleep(interval_t)
+
+task = multiprocessing.Process(target=GaoKaoTask)
+
+x = xiaoya('xiaoya', 17, 'telegram', '__all__')
 bot = telebot.TeleBot("121899714:AAF3xShKMc52iV5yN93fiIjOH98ZXP1zcOc")
-
-@bot.message_handler(commands=['start'])
-def send_welcome(msg):
-    if msg.chat.type == 'supergroup':
-        bot.reply_to(msg.chat.id, 'You send start to me.')
-
-@bot.message_handler(commands=['stop'])
-def send_welcome(msg):
-    if msg.chat.type == 'supergroup':
-        bot.reply_to(msg.chat.id, 'You send stop to me.')
-
-@bot.message_handler(commands=['schedule'])
-def handle(msg):
-    if msg.chat.type == 'supergroup':
-        bot.reply_to(msg.chat.id, 'You send schedule to me.')
 
 @bot.message_handler(commands=['chat_id'])
 def handle(msg):
     if msg.chat.type == 'supergroup':
-        bot.reply_to(msg.chat.id, 'Chat_id of this group:\n{}'.format(str(msg.chat.id)))
+        bot.reply_to(msg.chat.id, 'Chat_id of this group:\n\n{}'.format(str(msg.chat.id)))
+
+@bot.message_handler(commands=['start_task'])
+def handle(msg):
+    if msg.chat.type == 'supergroup':
+        if task.is_alive() == False:
+            task.start()
+
+@bot.message_handler(commands=['stop_task'])
+def handle(msg):
+    if msg.chat.type == 'supergroup':
+        if task.is_alive() == True:
+            task.terminate()
         
 @bot.message_handler(content_types=['text'])
 def handle(msg):
-    result = TGx.reply(msg.text)
+    result = x.reply(msg.text)
     if result != '':
         bot.reply_to(msg, result)
 
+task.start()
 bot.polling()
